d4ceb93d9c02f13fe6019eddd996b05a1b76c95e
Neel Raiyani <nraiyani@gmail.com>
2015-08-29 21:43:33 -0400

    Window Title updates

--- L7.Meshes/Main.cpp
+++ L7.Meshes/Main.cpp
@@ -112,7 +112,7 @@ int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, PWSTR pCmdLine
 	};
 
 
-	wnd = std::make_unique<Learnings::Window>(800, 500, L"L5. Transformations", Learnings::Window::Style::Windowed, callback);
+	wnd = std::make_unique<Learnings::Window>(800, 500, L"L7. Meshes", Learnings::Window::Style::Windowed, callback);
 	rndr = std::make_unique<Learnings::Renderer>(wnd->m_hWnd);
 	
 
