07d0b47feac32e4497f129b0bb14945f2a0534ac
Neel Raiyani <nraiyani@gmail.com>
2015-09-01 20:02:26 -0400

    L10 initial commit texture folder for other projects

---
+++ L10.BasicShapes/Main.cpp
@@ -0,0 +1,203 @@
+#include <memory>
+#include <iostream>
+#include <fstream>
+#include <Windows.h>
+
+#include "Main.h"
+
+#include "Window.h"
+#include "Renderer.h"
+#include "Mesh.h"
+
+std::vector<byte> ReadBinaryFile(const std::wstring &fileName)
+{
+	std::vector<byte> buffer;
+
+	std::ifstream inFile(fileName, std::ios::in | std::ios::binary);
+
+	if (!inFile.is_open())
+	{
+		std::runtime_error("Cannot open file");
+	}
+
+	buffer.assign((std::istreambuf_iterator<char>(inFile)), std::istreambuf_iterator<char>());
+
+	return buffer;
+}
+
+Learnings::Mesh TriangleMesh (float base, float height, float delta)
+{
+	float halfHeight = height / 2.0f;
+	float halfBase = base / 2.0f;
+
+	float x1 = -halfBase, y1 = -halfHeight,
+		x3 = base * delta, y3 = halfHeight,
+		x2 = halfBase, y2 = y1;
+
+	float oset = 0.5f + delta;
+	return {
+		// Vertex List
+		{
+			{ { x1, y1, 0.0f }, { 0.0f, 1.0f } },
+			{ { x3, y3, 0.0f }, { oset, 0.0f } },
+			{ { x2, y2, 0.0f }, { 1.0f, 1.0f } },
+		},
+
+		// Index List
+		{ 0, 1, 2 }
+	};
+}
+
+Learnings::Mesh GridMesh(float cellSize, uint32_t cellCount)
+{
+	using namespace Learnings;
+
+	auto startxy = cellSize * cellCount / 2.0f;
+	Vertex vStart{ {-startxy, 0.0f, -startxy}, {0.0f, 0.0f} };
+	Vertex vEnd{ { -startxy, 0.0f, startxy },{ 0.0f, 0.0f } };
+
+	Mesh grid;
+
+	// X Direction
+	for (uint32_t i = 0; i <= cellCount; i++)
+	{
+		uint32_t vertexCount = (uint32_t)grid.vertices.size();
+		grid.indices.insert(grid.indices.end(), { vertexCount, vertexCount + 1 });
+
+		Vertex v1 = vStart;
+		Vertex v2 = vEnd;
+
+		v1.position.x += i * cellSize;
+		v2.position.x += i * cellSize;
+
+		grid.vertices.insert(grid.vertices.end(), { v1, v2 });
+	}
+
+	vEnd = { { startxy, 0.0f, -startxy },{ 0.0f, 0.0f } };
+	// Z Direction
+	for (uint32_t i = 0; i <= cellCount; i++)
+	{
+		uint32_t vertexCount = (uint32_t)grid.vertices.size();
+		grid.indices.insert(grid.indices.end(), { vertexCount, vertexCount + 1 });
+
+		Vertex v1 = vStart;
+		Vertex v2 = vEnd;
+
+		v1.position.z += i * cellSize;
+		v2.position.z += i * cellSize;
+
+		grid.vertices.insert(grid.vertices.end(), { v1, v2 });
+	}
+
+	return grid;
+}
+
+int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, PWSTR pCmdLine, int nCmdShow)
+{
+	UNREFERENCED_PARAMETER(hPrevInstance);
+	CoInitializeEx(nullptr, COINITBASE_MULTITHREADED);
+
+	std::unique_ptr<Learnings::Window> wnd;
+	std::unique_ptr<Learnings::Renderer> rndr;
+
+	auto updateProjection = [&](float width, float height, float dFov) -> void
+	{
+		if (!rndr)
+			return;
+
+		float aspectRatio = width / height;
+		float hFov = DirectX::XMConvertToRadians(dFov);
+		float vFov = 2 * atan(tan(hFov / 2.0f) * aspectRatio);
+		auto prespective = DirectX::XMMatrixPerspectiveFovLH(vFov, 
+															aspectRatio, 
+															0.1f, 
+															1000.0f);
+		auto viewFrom = DirectX::XMMatrixLookAtLH({ {1.0f, 1.0f, -1.0f, 0.0f} },
+												  { {0.0f, 0.0f, 0.0f, 0.0f} },
+												  { { 0.0f, 1.0f, 0.0f, 0.0f } });
+		auto projection = viewFrom * prespective;
+		Learnings::Projection proj{ DirectX::XMMatrixTranspose(projection) };
+		rndr->SetProjection(proj);
+	};
+
+	auto callback = [&](Learnings::Window::Message msg, uint16_t v1, uint16_t v2) -> bool
+	{
+		if (!wnd)
+			return false;
+
+		using namespace Learnings;
+
+		switch (msg)
+		{
+			case Window::Activate:
+				break;
+			case Window::Deactivate:
+				break;
+			case Window::Pause:
+				break;
+			case Window::Resume:
+				break;
+			case Window::Resized:
+				wnd->Restyle(Window::Style::Windowed);
+				rndr->Resize();
+				updateProjection(v1, v2, 60.0f);
+				break;
+			case Window::Maximized:
+				wnd->Restyle(Window::Style::Fullscreen);
+				rndr->Resize();
+				updateProjection(v1, v2, 60.0f);
+				break;
+			case Learnings::Window::Close:
+				return true;
+				break;
+		}
+
+		return false;
+	};
+
+
+	wnd = std::make_unique<Learnings::Window>(800, 500, L"L10. Basic Shapes", Learnings::Window::Style::Windowed, callback);
+	rndr = std::make_unique<Learnings::Renderer>(wnd->m_hWnd);
+	
+	auto triangle = TriangleMesh(1.0f, 1.0f, 0.0f);
+	uint32_t triangleIdx = 0;
+	rndr->AddGeometry(triangleIdx, triangle);
+
+	auto grid = GridMesh(0.5f, 10);
+	uint32_t gridIdx = 1;
+	rndr->AddGeometry(gridIdx, grid);
+	rndr->SetTopology(gridIdx, D3D11_PRIMITIVE_TOPOLOGY_LINELIST);
+	
+	auto ms = DirectX::XMMatrixIdentity();
+	ms = DirectX::XMMatrixRotationAxis({{0.0f, 0.0f, 1.0f}}, 
+									   DirectX::XMConvertToRadians(90.0f));
+	Learnings::Transform transform{ DirectX::XMMatrixTranspose(ms) };
+	rndr->SetTransforms(triangleIdx, 0, transform);
+
+	ms = DirectX::XMMatrixIdentity();
+	ms = DirectX::XMMatrixRotationAxis({ { 0.0f, 0.0f, 1.0f } },
+									   DirectX::XMConvertToRadians(-90.0f));
+	transform = { DirectX::XMMatrixTranspose(ms) };
+	rndr->SetTransforms(triangleIdx, 1, transform);
+
+	ms = DirectX::XMMatrixTranslation(0.0f, 0.0f, 0.0f);
+	transform = { DirectX::XMMatrixTranspose(ms) };
+	rndr->SetTransforms(gridIdx, 0, transform);
+
+	auto vs = ReadBinaryFile(L"VertexShader.cso");
+	auto ps = ReadBinaryFile(L"PixelShader.cso");
+	rndr->AddShader(vs, ps);
+
+	auto texture = ReadBinaryFile(L"uv_grid.dds");
+	rndr->AddTexture(texture);
+
+	wnd->Show(nCmdShow);
+	// Main Loop
+	while (wnd->ProcessMessages())
+	{
+		rndr->Draw();
+	}
+
+	CoUninitialize();
+	return 0;
+}
---
+++ L10.BasicShapes/Window.cpp
@@ -0,0 +1,231 @@
+#include "Window.h"
+
+using namespace Learnings;
+
+//-------------------------------------------------------------------------------------
+// ATL Window stuff
+LRESULT Window::OnClose(UINT msg, WPARAM wParam, LPARAM lParam, BOOL &bHandled)
+{
+	if (m_Callback(Message::Close, 0, 0))
+	{
+		bHandled = DestroyWindow();
+	}
+
+	return 0;
+}
+
+LRESULT Window::OnDestroy(UINT msg, WPARAM wParam, LPARAM lParam, BOOL &bHandled)
+{
+	PostQuitMessage(0);
+
+	bHandled = TRUE;
+	return 0;
+}
+
+LRESULT Window::OnPaint(UINT msg, WPARAM wParam, LPARAM lParam, BOOL &bHandled)
+{
+	PAINTSTRUCT ps{ 0 };
+	HDC hdc = BeginPaint(&ps);
+	EndPaint(&ps);
+
+	bHandled = TRUE;
+	return 0;
+}
+
+LRESULT Window::OnActivate(UINT msg, WPARAM wParam, LPARAM lParam, BOOL &bHandled)
+{
+	if (wParam)
+	{
+		m_IsActive = true;
+		m_Callback(Message::Activate, 0, 0);
+	}
+	else
+	{
+		m_IsActive = false;
+		m_Callback(Message::Deactivate, 0, 0);
+	}
+
+	// TODO: Do Callback, let application know
+
+	bHandled = TRUE;
+	return 0;
+}
+
+LRESULT Window::OnResize(UINT msg, WPARAM wParam, LPARAM lParam, BOOL &bHandled)
+{
+	if (!m_IsResizing)
+	{
+		uint16_t w = (uint16_t)LOWORD(lParam);
+		uint16_t h = (uint16_t)HIWORD(lParam);
+
+		switch (wParam)
+		{
+			case SIZE_MINIMIZED:
+				break;
+			case SIZE_MAXIMIZED:
+				m_Callback(Message::Maximized, w, h);
+				break;
+			case SIZE_RESTORED:
+			default:
+				m_Callback(Message::Resized, w, h);
+				break;
+		}
+	}
+
+	bHandled = TRUE;
+	return 0;
+}
+
+LRESULT Window::OnSizing(UINT msg, WPARAM wParam, LPARAM lParam, BOOL &bHandled)
+{
+	if (m_hWnd)
+	{
+		switch (msg)
+		{
+			case WM_ENTERSIZEMOVE:
+				m_IsResizing = true;
+				m_Callback(Message::Pause, 0, 0);
+				break;
+			case WM_EXITSIZEMOVE:
+				m_IsResizing = false;
+				m_Callback(Message::Resume, 0, 0);
+				break;
+		}
+	}
+
+	bHandled = TRUE;
+	return 0;
+}
+
+LRESULT Learnings::Window::OnSystemCommand(UINT msg, WPARAM wParam, LPARAM lParam, BOOL & bHandled)
+{
+	if (wParam == SC_KEYMENU && lParam == VK_RETURN)
+	{
+		if (m_IsFullscreen)
+		{
+			ShowWindow(SW_RESTORE);
+		}
+		else
+		{
+			ShowWindow(SW_MAXIMIZE);
+		}
+	}
+
+	return DefWindowProc();
+}
+
+
+//-------------------------------------------------------------------------------------
+// Our Window stuff
+
+Window::Window(uint16_t width, uint16_t height, const std::wstring &title, Style style, const Callback &callback) :
+	m_Callback(callback)
+{
+	Create(nullptr,
+		   RECT{ 0, 0, width, height },
+		   title.c_str(),
+		   WS_OVERLAPPEDWINDOW,
+		   WS_EX_OVERLAPPEDWINDOW | WS_EX_LAYERED | WS_EX_COMPOSITED);
+
+	Restyle(style);
+	Resize(width, height);
+}
+
+Window::~Window()
+{
+}
+
+void Window::Restyle(Style style)
+{
+	DWORD adStyle = 0, adExStyle = 0;
+	DWORD rmStyle = 0, rmExStyle = 0;
+	switch (style)
+	{
+		case Window::Windowed:
+			m_IsFullscreen = false;
+			adStyle = WS_OVERLAPPEDWINDOW;
+			adExStyle = WS_EX_OVERLAPPEDWINDOW;
+			rmStyle = WS_POPUP | WS_MINIMIZEBOX;
+			break;
+		case Window::Borderless:
+			m_IsFullscreen = false;
+			adStyle = WS_POPUP | WS_MINIMIZEBOX;
+			rmStyle = WS_OVERLAPPEDWINDOW;
+			rmExStyle = WS_EX_OVERLAPPEDWINDOW;
+			break;
+		case Window::Fullscreen:
+			m_IsFullscreen = true;
+			adStyle = WS_POPUP | WS_MINIMIZEBOX;
+			rmStyle = WS_OVERLAPPEDWINDOW;
+			rmExStyle = WS_EX_OVERLAPPEDWINDOW;
+			break;
+	}
+
+	ModifyStyle(rmStyle, adStyle);
+	ModifyStyleEx(rmExStyle, adExStyle);
+	CenterWindow();
+	
+	if (m_IsFullscreen)
+	{
+		MONITORINFO mi { sizeof(MONITORINFO) };
+		GetMonitorInfo(MonitorFromWindow(m_hWnd, MONITOR_DEFAULTTONEAREST), &mi);
+
+		BOOL bRet = SetWindowPos(HWND_TOP,
+								 &mi.rcMonitor,
+								 SWP_NOOWNERZORDER | SWP_FRAMECHANGED);
+	}
+}
+
+void Window::Resize(uint16_t width, uint16_t height)
+{
+	if (!m_IsFullscreen)
+	{
+		ResizeClient(width, height);
+		CenterWindow();
+	}
+}
+
+void Window::Retitle(const std::wstring &title)
+{
+	SetWindowText(title.c_str());
+}
+
+void Window::Show(int showCmd)
+{
+	if (m_hWnd)
+	{
+		ShowWindow(showCmd);
+	}
+}
+
+bool Window::ProcessMessages()
+{
+	MSG msg{ 0 };
+	BOOL bRet = TRUE;
+
+	do
+	{
+		if (m_IsActive)
+		{
+			bRet = PeekMessage(&msg, nullptr, 0, 0, PM_REMOVE);
+		}
+		else
+		{
+			bRet = GetMessage(&msg, nullptr, 0, 0);
+		}
+
+		if (bRet)
+		{
+			TranslateMessage(&msg);
+			DispatchMessage(&msg);
+		}
+
+		if (msg.message == WM_QUIT)
+		{
+			return false;
+		}
+
+	} while (bRet);
+
+	return true;
+}
